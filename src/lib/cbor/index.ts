adc98846c6e9e5263a6d831d61a017b616085b77
Jarosław Leśniak <jarek9244@gmail.com>
2024-08-10 21:07:58 +0200

    Refactor cbor directory

--- src/lib/cbor/index.ts
+++ src/lib/cbor/index.ts
@@ -1,36 +1,6 @@
 import { CBORType, decodePartialCBOR, encodeCBOR } from "@levischuck/tiny-cbor";
 
-/**
- * Whatever CBOR encoder is used should keep CBOR data the same length when data is re-encoded
- *
- * MOST CRITICALLY, this means the following needs to be true of whatever CBOR library we use:
- * - CBOR Map type values MUST decode to JavaScript Maps
- * - CBOR tag 64 (uint8 Typed Array) MUST NOT be used when encoding Uint8Arrays back to CBOR
- *
- * So long as these requirements are maintained, then CBOR sequences can be encoded and decoded
- * freely while maintaining their lengths for the most accurate pointer movement across them.
- */
+export const decodeFirst = <T>(input: Uint8Array): T =>
+  (decodePartialCBOR(new Uint8Array(input), 0) as [T, number])[0];
 
-/**
- * Decode and return the first item in a sequence of CBOR-encoded values
- *
- * @param input The CBOR data to decode
- * @param asObject (optional) Whether to convert any CBOR Maps into JavaScript Objects. Defaults to
- * `false`
- */
-export function decodeFirst<Type>(input: Uint8Array): Type {
-  // Make a copy so we don't mutate the original
-  const _input = new Uint8Array(input);
-  const decoded = decodePartialCBOR(_input, 0) as [Type, number];
-
-  const [first] = decoded;
-
-  return first;
-}
-
-/**
- * Encode data to CBOR
- */
-export function encode(input: CBORType): Uint8Array {
-  return encodeCBOR(input);
-}
+export const encode = (input: CBORType): Uint8Array => encodeCBOR(input);
