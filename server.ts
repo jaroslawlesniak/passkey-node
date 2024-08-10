6803ced43f5e1601ab94006d06f006142ccf0c9b
Jarosław Leśniak <jarek9244@gmail.com>
2024-08-10 11:44:49 +0200

    Simplified code

--- server.ts
+++ server.ts
@@ -5,11 +5,7 @@ import cors from "cors";
 
 const app: Express = express();
 
-app.use(
-  cors({
-    origin: "*",
-  }),
-);
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
