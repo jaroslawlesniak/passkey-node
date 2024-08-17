1746ff987388e9d0c7a9bf972d29d82c3c96fcad
Jarosław Leśniak <jarek9244@gmail.com>
2024-08-17 12:08:50 +0200

    Refactor jwt

--- src/lib/jwt/index.ts
+++ src/lib/jwt/index.ts
@@ -3,13 +3,13 @@ import jwt from "jsonwebtoken";
 
 import { Token, TokenPayload } from "./types";
 
-const secret = "some secret value";
-const expiresIn = "1d";
+const SECRET = "some secret value"; // should be stored somewhere in a secured way
+const TOKEN_EXPIRES_IN = "1d";
 
 export const sign = <T extends object>(userToken: string, payload: T): Token =>
-  jwt.sign({ ...payload, userToken }, secret, { expiresIn });
+  jwt.sign({ ...payload, userToken }, SECRET, { expiresIn: TOKEN_EXPIRES_IN });
 
-export const validate = (token: Token) => jwt.verify(token, secret);
+export const validate = (token: Token) => jwt.verify(token, SECRET);
 
 export const extract = (req: Request) => {
   const token = req.body.token;
