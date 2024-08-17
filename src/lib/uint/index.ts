5cbfba71f0207359d2102278639dea2ee0b3c6f7
Jarosław Leśniak <jarek9244@gmail.com>
2024-08-17 12:47:21 +0200

    Add empty array constant

--- src/lib/uint/index.ts
+++ src/lib/uint/index.ts
@@ -10,10 +10,11 @@ export const toHex = (array: Uint8Array): string =>
   Array.from(array, (i) => i.toString(16).padStart(2, "0")).join("");
 
 const HEX_STRING_REGEX = /[^a-fA-F0-9]/u;
+const EMPTY_ARRAY = [] as const;
 
 export const fromHex = (hex?: string): Uint8Array => {
   if (!hex) {
-    return Uint8Array.from([]);
+    return Uint8Array.from(EMPTY_ARRAY);
   }
 
   const valid =
@@ -23,7 +24,7 @@ export const fromHex = (hex?: string): Uint8Array => {
     throw new Error("Invalid hex string");
   }
 
-  const bytes = hex.match(/.{1,2}/g) ?? [];
+  const bytes = hex.match(/.{1,2}/g) ?? EMPTY_ARRAY;
 
   return Uint8Array.from(bytes.map((byte) => parseInt(byte, 16)));
 };
