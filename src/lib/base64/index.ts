57a7bd569a4a800cba8ceedaa2fe8cce0d9308f5
Jarosław Leśniak <jarek9244@gmail.com>
2024-08-24 21:36:38 +0200

    Rename function for detecting url mode

--- src/lib/base64/index.ts
+++ src/lib/base64/index.ts
@@ -4,34 +4,34 @@ import type { Base64URLString } from "@/lib/passkey";
 
 import type { Algorithm, Utf8String } from "./types";
 
-const toUrlMode = (origin: Algorithm) => origin === "base64url";
+const isUrlMode = (origin: Algorithm) => origin === "base64url";
 
 export const toBuffer = (
   payload: string,
   from: Algorithm = "base64url",
-): Uint8Array => new Uint8Array(base64.toArrayBuffer(payload, toUrlMode(from)));
+): Uint8Array => new Uint8Array(base64.toArrayBuffer(payload, isUrlMode(from)));
 
 export const fromBuffer = (
   buffer: Uint8Array,
   to: Algorithm = "base64url",
-): string => base64.fromArrayBuffer(buffer, toUrlMode(to));
+): string => base64.fromArrayBuffer(buffer, isUrlMode(to));
 
 export const toBase64 = (payload: Base64URLString): Promise<string> =>
   Promise.resolve()
-    .then(() => base64.toArrayBuffer(payload, toUrlMode("base64url")))
-    .then((buffer) => base64.fromArrayBuffer(buffer, toUrlMode("base64")));
+    .then(() => base64.toArrayBuffer(payload, isUrlMode("base64url")))
+    .then((buffer) => base64.fromArrayBuffer(buffer, isUrlMode("base64")));
 
 export const fromUTF8String = (payload: Utf8String): string =>
-  base64.fromString(payload, toUrlMode("base64url"));
+  base64.fromString(payload, isUrlMode("base64url"));
 
 export const toUTF8String = (payload: Base64URLString): string =>
-  base64.toString(payload, toUrlMode("base64url"));
+  base64.toString(payload, isUrlMode("base64url"));
 
 export const isBase64 = (payload: string): boolean =>
-  base64.validate(payload, toUrlMode("base64"));
+  base64.validate(payload, isUrlMode("base64"));
 
 export const isBase64URL = (payload: string): boolean =>
-  base64.validate(trimPadding(payload), toUrlMode("base64url"));
+  base64.validate(trimPadding(payload), isUrlMode("base64url"));
 
 export const trimPadding = (payload: Base64URLString): Base64URLString =>
   payload.replace(/=/g, "");
