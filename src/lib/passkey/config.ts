278390f8d9dbeb31189049985c861c9688f08436
Jarosław Leśniak <jarek9244@gmail.com>
2024-08-24 21:39:31 +0200

    Rename app name

--- src/lib/passkey/config.ts
+++ src/lib/passkey/config.ts
@@ -1,7 +1,7 @@
 export const ES256 = -7;
 export const RS256 = -257;
 
-export const rpName: string = "Passkeys Tutorial";
+export const rpName: string = "Passkey";
 export const rpID: string = "localhost";
 export const rpId: string = "localhost";
 export const origin: string = `http://${rpID}:5173`;
